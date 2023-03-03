da243a2b815bc3f64a02c6ac2d25d49945d141d2
Gabriel Brilhante <joao.brilhante@alu.ufc.br>
2023-03-03 13:07:35 -0300

    feat: Alterando cor de Loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,7 +3,7 @@ import { Box, CircularProgress } from "@mui/material";
 export function Loading() {
    return (
       <Box height="100vh" display="flex" justifyContent="center" alignItems="center" >
-         <CircularProgress color="info" />
+         <CircularProgress color="success" />
       </Box >
    );
 }
