d0816477118e97b05881c443165d44c34823fdf4
Gabriel Brilhante <joao.brilhante@alu.ufc.br>
2023-03-03 13:06:35 -0300

    feat: Adicionando um switch aos favoritos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,31 @@
 import SearchIcon from '@mui/icons-material/Search';
-import { Container, IconButton, InputAdornment, TextField } from "@mui/material";
+import { Button, Container, FormControlLabel, IconButton, InputAdornment, Switch, TextField } from "@mui/material";
 import { useState } from "react";
 import CountriesGrid from "../../components/CountriesGrid";
 
 export function Home() {
    const [textValue, setTextValue] = useState("");
    const [search, setSearch] = useState("");
+   const [filteringFavorites, setFilteringFavorites] = useState(false);
 
    function handleSearch() {
       setSearch(textValue);
    }
 
+   function handleFilterForFavorite() {
+      setFilteringFavorites(prevState => !prevState);
+   }
+
    return (
-      <Container>
+      <Container sx={{ color: "#FFF" }}>
          <TextField
             id="searchBar"
-            placeholder="Pesquise um país"
+            placeholder="Search for a country"
             value={textValue}
             variant="outlined"
+            fullWidth
+            sx={{ borderRadius: 2, margin: "1rem 0", backgroundColor: "#FFF" }}
+            onChange={(e) => setTextValue(e.target.value)}
             InputProps={{
                endAdornment: (
                   <InputAdornment position="end">
@@ -27,11 +35,25 @@ export function Home() {
                   </InputAdornment>
                ),
             }}
-            fullWidth
-            sx={{ borderRadius: 2, margin: "1rem 0", backgroundColor: "#FFF" }}
-            onChange={(e) => setTextValue(e.target.value)}
          />
-         <CountriesGrid countrySearch={search} />
+
+         <FormControlLabel
+            control={
+               <Switch
+                  color='success'
+                  checked={filteringFavorites}
+                  onChange={handleFilterForFavorite}
+                  inputProps={{ 'aria-label': 'controlled' }}
+               />
+            }
+            label="Filter favorites"
+            labelPlacement='start'
+            sx={{
+               marginBottom: "1rem"
+            }}
+         />
+
+         <CountriesGrid countrySearch={search} filteringFavorites={filteringFavorites} />
       </Container>
    );
 }
